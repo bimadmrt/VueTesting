d979cac9cafb4042989e987de061d21e2c2059e9
bimadmrt <56182626+bimadmrt@users.noreply.github.com>
2020-05-15 23:48:24 +0700

    final

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,11 +5,40 @@
                 <img src="@/assets/3.png" class="img-fluid"/>
             </div>
             <div class="col-md-6 p-4">
-                <h1 class="mb-5">ABOUT ME<span class="text-danger">.</span></h1>
+                <h1 class="mb-5">The Best Food in The World<span class="text-danger">.</span></h1>
+                <h3 class="mb-5">Rendang</h3>
                 <p class="text-justify">
-                    Hello my name is Bima Demarta Aisy and it's my Meal Blog Project.
-                    This website is about Meal API Website. Inside it, there is a
-                    food catalog page, category, meals area, and many more.
+                    Rendang atau randang adalah masakan daging dengan bumbu
+                    rempah-rempah yang berasal dari Minangkabau. Masakan ini
+                    dihasilkan dari proses memasak yang dipanaskan berulang-ulang
+                    menggunakan santan sampai kuahnya kering sama sekali
+                </p>
+            </div>
+        </div>
+        <div class="row row-about p-5">
+            <div class="col-md-6 p-4">
+                <img src="@/assets/4.png" class="img-fluid"/>
+            </div>
+            <div class="col-md-6 p-4">
+                <h1 class="mb-5">The Best Restaurant in The World<span class="text-danger">.</span></h1>
+                <h3 class="mb-5">Osteria Francescana</h3>
+                <p class="text-justify">
+                    Osteria Francescana adalah restoran yang dimiliki dan dijalankan oleh
+                    koki Massimo Bottura di Modena, Italia. Pada 2018, Osteria Francescana
+                    dinobatkan sebagai restoran terbaik di dunia dalam 50 Restoran Terbaik Dunia
+                </p>
+            </div>
+        </div>
+        <div class="row row-about p-5">
+            <div class="col-md-6 p-4">
+                <img src="@/assets/5.png" class="img-fluid"/>
+            </div>
+            <div class="col-md-6 p-4">
+                <h1 class="mb-5">The Best Chef in The World<span class="text-danger">.</span></h1>
+                <h3 class="mb-5">Chef Mauro Colagreco</h3>
+                <p class="text-justify">
+                    Mauro Colagreco adalah koki Argentina Italia di restoran
+                    bintang tiga Michelin Mirazur di Menton, Prancis.
                 </p>
             </div>
         </div>
